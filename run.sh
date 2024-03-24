60e9142d2c24b00cd22cb70c75ed84d0a2b98275
sy1vi3 <me@sylvie.fyi>
2024-03-24 14:03:57 -0400

    0.0.2

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget https://github.com/UnownHash/Xilriws-Public/releases/download/xilriws-v0.0.1/xilriws
-wget https://github.com/UnownHash/Xilriws-Public/releases/download/xilriws-v0.0.1/config.json.example
+wget https://github.com/UnownHash/Xilriws-Public/releases/download/xilriws-v0.0.2/xilriws
+wget https://github.com/UnownHash/Xilriws-Public/releases/download/xilriws-v0.0.2/config.json.example
 chmod +x xilriws
 cp config.json.example config.json
 
