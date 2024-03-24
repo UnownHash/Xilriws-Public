7306b4c7b4fff8e228ca3b2cbf50ebff5d14652d
sy1vi3 <me@sylvie.fyi>
2024-03-24 18:20:05 -0400

    dockerfile with jinna's changes

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
 FROM ubuntu:latest
 RUN apt update -y && \
-    apt install -y chromium-driver && \
+    apt install -y wget && \
     apt clean
+
+RUN wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+RUN apt-get install -y ./google-chrome-stable_current_amd64.deb
 
 WORKDIR /xilriws
 
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://github.com/UnownHash/Xilriws-Public/releases/download/xilriws-v0.0.2/xilriws
+wget https://github.com/UnownHash/Xilriws-Public/releases/download/xilriws-v0.0.2-alpine/xilriws
 wget https://github.com/UnownHash/Xilriws-Public/releases/download/xilriws-v0.0.2/config.json.example
 chmod +x xilriws
 cp config.json.example config.json
