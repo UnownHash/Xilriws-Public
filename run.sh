673bb7acd5dbf9463df7373f78d8cfbcfb3b889f
Malte <42342921+ccev@users.noreply.github.com>
2024-03-26 17:51:45 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget https://github.com/UnownHash/Xilriws-Public/releases/download/xilriws-v0.1.1/xilriws -O xilriws
-wget https://github.com/UnownHash/Xilriws-Public/releases/download/xilriws-v0.1.1/config.json.example -O config.json.example
+wget https://github.com/UnownHash/Xilriws-Public/releases/download/xilriws-v0.1.2/xilriws -O xilriws
+wget https://github.com/UnownHash/Xilriws-Public/releases/download/xilriws-v0.1.2/config.json.example -O config.json.example
 chmod +x xilriws
 cp config.json.example xilriws.json
 
