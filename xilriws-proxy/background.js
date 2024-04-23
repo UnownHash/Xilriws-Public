6dff69273edf1f1636b4babfb2ec3cff86c708d4
Malte <42342921+ccev@users.noreply.github.com>
2024-04-23 02:38:13 +0200

    singleproxy -> proxyforhttp

--- xilriws-proxy/background.js
+++ xilriws-proxy/background.js
@@ -22,7 +22,7 @@ function startProxy(host, port) {
     const config = {
         mode: "fixed_servers",
         rules: {
-            singleProxy: {
+            proxyForHttp: {
                 scheme: "http",
                 host: host,
                 port: port,
