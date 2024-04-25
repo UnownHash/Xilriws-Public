f39c9a03057e30bd077f8305e6057a3fa0669f4e
Malte <42342921+ccev@users.noreply.github.com>
2024-04-25 03:36:26 +0200

    quote ua

--- xilriws/browser.py
+++ xilriws/browser.py
@@ -69,7 +69,7 @@ class Browser:
         if not self.browser:
             config = nodriver.Config(headless=HEADLESS, browser_executable_path=self.__find_chrome_executable())
             config.add_argument(
-                "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.3"
+                "--user-agent=\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.3\""
             )
             full_command = f"{config.browser_executable_path} {' '.join(config())}"
 
