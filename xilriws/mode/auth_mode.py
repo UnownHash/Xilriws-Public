bc356dbc337e202b22a44018746d5050d47db94d
Jakub <10072920+lenisko@users.noreply.github.com>
2024-05-26 23:25:45 +0200

    Update auth_mode.py

--- xilriws/mode/auth_mode.py
+++ xilriws/mode/auth_mode.py
@@ -39,11 +39,6 @@ class ResponseData:
 
 @post("/api/v1/login-code")
 async def auth_endpoint(request: Request, ptc_auth: PtcAuth, data: RequestData) -> Response[ResponseData]:
-    user_agent = request.headers.get("User-Agent", "")
-    if user_agent not in ("Go-http-client/1.1", "axios/1.6.8") or not user_agent.startswith("Dragonite/"):
-        logger.critical("Please use Dragonite or TMS to connect to Xilriws")
-        return Response(ResponseData(status=ResponseStatus.ERROR.name), status_code=HTTP_403_FORBIDDEN)
-
     try:
         login_code = await ptc_auth.auth(data.username, data.password, data.url)
 
